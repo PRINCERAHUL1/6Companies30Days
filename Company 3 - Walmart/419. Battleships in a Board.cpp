5d0d9e01e9d58950c0fb71bd9d1bb363341511c1
Rahul Mandal <rmrahul258@gmail.com>
2025-01-11 22:58:18 +0530

    Day 11

---
+++ Company 3 - Walmart/419. Battleships in a Board.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int rows = board.size();
+        int cols = board[0].size();
+        int count = 0;
+
+        for(int i=0; i<rows; i++)
+        {
+            for(int j=0; j<cols; j++)
+            {
+                if(board[i][j] == 'X')
+                {
+                    if(i>0 && board[i-1][j] == 'X' || j>0 && board[i][j-1] == 'X')
+                        continue;
+                    
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
